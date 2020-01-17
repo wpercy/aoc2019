a5f0bc7398370edb8cc52615661614b6354cd6e3
Will Percival <wpercy27@gmail.com>
2020-01-17 14:48:54 -0500

    make fxns

--- day1.py
+++ day1.py
@@ -1,20 +1,21 @@
 # DAY ONE (1)
 import math
-x = map(int, _input.split('\n'))
-_sum = 0
-for y in x:
-    _sum += (math.floor(y/3) - 2)
-print _sum
+def day1a(_input)
+    x = map(int, _input.split('\n'))
+    _sum = 0
+    for y in x:
+        _sum += (math.floor(y/3) - 2)
+    return _sum
 
 # DAY ONE (2)
 import math
-x = map(int, _input.split('\n'))
-_sum = 0
-for y in x:
-    new_fuel = math.floor(y/3) - 2
-    _sum += new_fuel
-    while new_fuel > 0:
-        new_fuel = math.floor(new_fuel/3) - 2
-        if new_fuel > 0:
-            _sum += new_fuel
-print _sum
+    x = map(int, _input.split('\n'))
+    _sum = 0
+    for y in x:
+        new_fuel = math.floor(y/3) - 2
+        _sum += new_fuel
+        while new_fuel > 0:
+            new_fuel = math.floor(new_fuel/3) - 2
+            if new_fuel > 0:
+                _sum += new_fuel
+    return _sum
