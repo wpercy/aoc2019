96050e615ae18bdcab638a58a2f7e0f23d789dfc
Will Percival <wpercy27@gmail.com>
2020-01-24 14:18:53 -0500

    rm unused fxn

--- day2.py
+++ day2.py
@@ -69,30 +69,6 @@ def day2b(_input):
                 return (100 * noun + verb)
 
 
-def day2b_again(_input):
-    for noun in range(100):
-        for verb in range(100):
-            x = map(int, _input.split(','))
-            x[1] = noun
-            x[2] = verb
-            pos = 0
-            while x[pos] != 99:
-                op = x[pos]
-                new_pos = x[pos+3]
-
-                a = x[x[pos+1]]
-                b = x[x[pos+2]]
-
-                if op == 1:
-                    ans = a + b
-                elif op == 2:
-                    ans = a * b
-
-                x[new_pos] = ans
-                pos += 4
-            if x[0] == 19690720:
-                return (100 * noun + verb)
-
 if __name__ == "__main__":
     with open('data/day2.txt') as f:
         _input = f.read()
